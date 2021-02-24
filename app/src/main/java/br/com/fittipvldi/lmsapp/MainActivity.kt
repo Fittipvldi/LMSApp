1fc43ab1181c76bc7c6d39671760bc77b5b49bb0
Fittipvldi <henrique.gfitti@gmail.com>
2021-02-24 19:05:57 -0300

    aula01

--- app/src/main/java/br/com/fittipvldi/lmsapp/MainActivity.kt
+++ app/src/main/java/br/com/fittipvldi/lmsapp/MainActivity.kt
@@ -2,10 +2,13 @@ package br.com.fittipvldi.lmsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
+
+        imagem_login.setImageResource(R.drawable.imagem_login)
     }
 }
