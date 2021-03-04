79b07d7d1f12bda4d8702202768dbc01bb39f9af
Fittipvldi <henrique.gfitti@gmail.com>
2021-03-04 00:27:17 -0300

    aula03

--- app/src/main/java/br/com/fittipvldi/lmsapp/TelaInicialActivity.kt
+++ app/src/main/java/br/com/fittipvldi/lmsapp/TelaInicialActivity.kt
@@ -3,6 +3,8 @@ package br.com.fittipvldi.lmsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.login.*
 
@@ -14,5 +16,28 @@ class TelaInicialActivity : DebugActivity() {
         var params = intent.extras
         val nome = params?.getString("nome")
         Toast.makeText(this, "Bem-vindo $nome", Toast.LENGTH_SHORT).show()
+
+        supportActionBar?.title = "Disciplinas"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if(id == R.id.action_buscar) {
+            Toast.makeText(this, "Clicou Buscar!", Toast.LENGTH_SHORT).show()
+        } else if(id == R.id.action_atualizar) {
+            Toast.makeText(this, "Clicou em Atualizar!", Toast.LENGTH_SHORT).show()
+        } else if(id == R.id.action_config) {
+            Toast.makeText(this, "Clicou em Configurações!", Toast.LENGTH_SHORT).show()
+        } else if(id == android.R.id.home) {
+            finish()
+        }
+        return  super.onOptionsItemSelected(item)
     }
 }
